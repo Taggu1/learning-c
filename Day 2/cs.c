da99b99cd58304e9fcb0462fa1aa5b6630005929
taggu1 <hishammohammed@Hishams-MacBook-Air.local>
2025-01-14 09:11:36 +0200

    Test commit

---
+++ Day 2/cs.c
@@ -0,0 +1,17 @@
+
+// Name: cs.c
+// Purpose: Testing conversion specifications
+// Author: Hisham Mohammed
+
+#include <stdio.h>
+
+int main(void)
+{
+    float num = 100;
+
+    printf("First Test: %5d\n", num);
+    printf("First Test: %5f\n", num);
+
+    printf("Second Test: %f\n", num);
+    printf("Second Test: %5f\n", num);
+}
